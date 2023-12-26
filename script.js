f21b26df571836f3322e1c1c07bc96dee7991566
yash <yash110820@gmail.com>
2023-12-26 17:21:00 +0530

    added Edit Button

--- script.js
+++ script.js
@@ -16,14 +16,27 @@ function updateList() {
   arr.forEach((item, index) => {
     let li = document.createElement("li");
     li.innerText = item;
-
+    //Added Edit Button
+    let edit = document.createElement("button");
+    edit.innerText = "edit";
+    edit.addEventListener("click", () => {
+      document.getElementById("input").value = arr.find(
+        (item, i) => i == index
+      );
+      edit.style.display = "flex";
+      arr.splice(index, 1);
+      updateList();
+    });
+    //Todo Stricking or Completed
     li.addEventListener("click", function () {
       li.style.textDecoration = "line-through";
     });
+    //Removing the Todos
     li.addEventListener("dblclick", function () {
       arr.splice(index, 1);
       updateList();
     });
     todo.appendChild(li);
+    todo.append(edit);
   });
 }
